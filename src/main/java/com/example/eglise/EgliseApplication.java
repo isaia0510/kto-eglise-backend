3a260e3c0dae4916b9586fc3a017f7d4aa08576a
isaia0510 <977jvmmwbt@privaterelay.appleid.com>
2026-04-14 12:57:08 +0300

    Initial commit - Spring Boot backend

---
+++ src/main/java/com/example/eglise/EgliseApplication.java
@@ -0,0 +1,13 @@
+package com.example.eglise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgliseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eglise/EgliseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eglise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
